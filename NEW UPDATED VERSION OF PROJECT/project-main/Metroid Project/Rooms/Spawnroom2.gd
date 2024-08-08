6aa3916197ba409b8f0ae9ee4e8e4f89c5f31c13
Omtegu <121986962+Omtegu@users.noreply.github.com>
2024-08-08 14:33:23 -0700

    Add files via upload

---
+++ NEW UPDATED VERSION OF PROJECT/project-main/Metroid Project/Rooms/Spawnroom2.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+func _on_door_12_body_entered(body):
+	queue_redraw()
+
+
+
+func _on_door_34_area_entered(area):
+	queue_free()
+	pass # Replace with function body.
+
+
+func _on_door_23_area_entered(area):
+	queue_redraw()
+	pass # Replace with function body.
